05da28a6ca9fd13a8392581d36aa8562da113fa1
cyoq <di9akoz@gmail.com>
2023-02-27 17:59:16 +0200

    Add From trait to Move

--- src/game/move.rs
+++ src/game/move.rs
@@ -1,4 +1,18 @@
+use crate::common::from_2d_to_bitboard;
+
+#[derive(Debug, PartialEq, Eq)]
 pub struct Move {
     pub from: u32,
     pub to: u32,
+}
+
+impl From<[(u32, u32); 2]> for Move {
+    fn from(value: [(u32, u32); 2]) -> Self {
+        let from = value[0];
+        let to = value[1];
+        Self {
+            from: from_2d_to_bitboard(from),
+            to: from_2d_to_bitboard(to),
+        }
+    }
 }
