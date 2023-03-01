bceced91e87158f48e899a0115e32a122ba53f0a
cyoq <di9akoz@gmail.com>
2023-03-01 20:49:44 +0200

    Check win condition for the king. Fix tests

--- src/game/state.rs
+++ src/game/state.rs
@@ -45,8 +45,9 @@ pub const BLUE_KING_IDX: usize = 1;
 pub const RED_PAWNS_IDX: usize = 0;
 pub const BLUE_PAWNS_IDX: usize = 1;
 
-// pub const RED_PLAYER_IDX: usize = 0;
-// pub const BLUE_PLAYER_IDX: usize = 1;
+// Indexes where the temples are
+pub const BLUE_TEMPLE: usize = 2;
+pub const RED_TEMPLE: usize = 22;
 
 #[derive(Debug, Clone)]
 pub struct State {
@@ -108,7 +109,12 @@ impl State {
     }
 
     /// When making a move, we assume that the move is completely legal by rules
-    pub fn make_move(&mut self, mov: &Move, player_color: &PlayerColor, card: &Card) -> MoveResult {
+    pub fn make_move(
+        &mut self,
+        mov: &Move,
+        player_color: PlayerColor,
+        used_card_idx: usize,
+    ) -> MoveResult {
         let from = mov.from as usize;
         let to = mov.to as usize;
         let figure = mov.figure;
@@ -116,8 +122,8 @@ impl State {
 
         // Clear 'from' position
         match figure {
-            Figure::Pawn => clear_bit(&mut self.pawns[*player_color as usize], from),
-            Figure::King => clear_bit(&mut self.kings[*player_color as usize], from),
+            Figure::Pawn => clear_bit(&mut self.pawns[player_color as usize], from),
+            Figure::King => clear_bit(&mut self.kings[player_color as usize], from),
         }
 
         // Check if there is a capture of an enemy piece
@@ -135,32 +141,40 @@ impl State {
             // Clear enemy bit
             clear_bit(&mut self.kings[enemy as usize], to);
             // Set a move result
-            match *player_color {
+            match player_color {
                 PlayerColor::Red => move_result = MoveResult::RedWin,
                 PlayerColor::Blue => move_result = MoveResult::BlueWin,
             }
         }
 
-        // Set a new 'to' position
+        // Set a piece to the 'to' position
         match figure {
-            Figure::Pawn => set_bit(&mut self.pawns[*player_color as usize], to),
-            Figure::King => set_bit(&mut self.kings[*player_color as usize], to),
+            Figure::Pawn => set_bit(&mut self.pawns[player_color as usize], to),
+            Figure::King => set_bit(&mut self.kings[player_color as usize], to),
         }
 
         // If it is a king, check if it is coming to the temple
+        if figure == Figure::King {
+            match (player_color, to) {
+                (PlayerColor::Red, BLUE_TEMPLE) => move_result = MoveResult::RedWin,
+                (PlayerColor::Blue, RED_TEMPLE) => move_result = MoveResult::BlueWin,
+                _ => (),
+            }
+        }
 
         // Card rotation:
+        self.deck.rotate(used_card_idx, player_color);
 
         move_result
     }
 
     /// Generates all legal moves for the specific card and the player
-    pub fn generate_legal_moves(&self, player_color: &PlayerColor, card: &Card) -> Vec<Move> {
+    pub fn generate_legal_moves(&self, player_color: PlayerColor, card: &Card) -> Vec<Move> {
         let mut result: Vec<Move> = Vec::new();
         // Save pawns for the specific player
-        let pawns: u32 = self.pawns[*player_color as usize];
+        let pawns: u32 = self.pawns[player_color as usize];
         // Save king for the specific player
-        let king: u32 = self.kings[*player_color as usize];
+        let king: u32 = self.kings[player_color as usize];
 
         for n in 0..25 {
             // Getting a position bit for a pawn
@@ -174,7 +188,7 @@ impl State {
             }
 
             // Get attack map for the specific player, card and the position
-            let attack_map = ATTACK_MAPS[*player_color as usize][card.index][n];
+            let attack_map = ATTACK_MAPS[player_color as usize][card.index][n];
 
             let map: u32;
             let figure: Figure;
@@ -256,7 +270,7 @@ mod tests {
         let deck = Deck::new([CRAB, RABBIT, DRAGON, TIGER, FROG]);
         let state = State::with_deck(deck);
 
-        let cards = state.deck.get_player_cards(&PlayerColor::Red);
+        let cards = state.deck.get_player_cards(PlayerColor::Red);
         let crab = cards[0];
 
         let mut crab_expected_moves = vec![
@@ -268,7 +282,7 @@ mod tests {
             Move::from(([(4, 3), (3, 3)], Figure::Pawn)),
             Move::from(([(4, 4), (3, 4)], Figure::Pawn)),
         ];
-        let mut crab_moves = state.generate_legal_moves(&PlayerColor::Red, crab);
+        let mut crab_moves = state.generate_legal_moves(PlayerColor::Red, crab);
         crab_moves.sort();
         crab_expected_moves.sort();
         assert_eq!(crab_moves, crab_expected_moves);
@@ -282,7 +296,7 @@ mod tests {
             Move::from(([(4, 2), (3, 3)], Figure::King)),
             Move::from(([(4, 3), (3, 4)], Figure::Pawn)),
         ];
-        let mut rabbit_moves = state.generate_legal_moves(&PlayerColor::Red, rabbit);
+        let mut rabbit_moves = state.generate_legal_moves(PlayerColor::Red, rabbit);
         rabbit_moves.sort();
         rabbit_expected_moves.sort();
         assert_eq!(rabbit_moves, rabbit_expected_moves);
@@ -293,7 +307,7 @@ mod tests {
         let deck = Deck::new([DRAGON, TIGER, CRAB, RABBIT, FROG]);
         let state = State::with_deck(deck);
 
-        let cards = state.deck.get_player_cards(&PlayerColor::Blue);
+        let cards = state.deck.get_player_cards(PlayerColor::Blue);
 
         let crab = cards[0];
 
@@ -306,7 +320,7 @@ mod tests {
             Move::from(([(0, 3), (1, 3)], Figure::Pawn)),
             Move::from(([(0, 4), (1, 4)], Figure::Pawn)),
         ];
-        let mut crab_moves = state.generate_legal_moves(&PlayerColor::Blue, crab);
+        let mut crab_moves = state.generate_legal_moves(PlayerColor::Blue, crab);
         crab_moves.sort();
         crab_expected_moves.sort();
         assert_eq!(crab_moves, crab_expected_moves);
@@ -320,7 +334,7 @@ mod tests {
             Move::from(([(0, 3), (1, 2)], Figure::Pawn)),
             Move::from(([(0, 4), (1, 3)], Figure::Pawn)),
         ];
-        let mut rabbit_moves = state.generate_legal_moves(&PlayerColor::Blue, rabbit);
+        let mut rabbit_moves = state.generate_legal_moves(PlayerColor::Blue, rabbit);
         rabbit_moves.sort();
         rabbit_expected_moves.sort();
         assert_eq!(rabbit_moves, rabbit_expected_moves);
@@ -331,7 +345,7 @@ mod tests {
         let deck = Deck::new([CRAB, RABBIT, DRAGON, TIGER, FROG]);
         let mut state = State::with_deck(deck);
 
-        let cards = state.deck.get_player_cards(&PlayerColor::Red);
+        let cards = state.deck.get_player_cards(PlayerColor::Red);
         // Cloning to avoid immutable borrow before mutable
         let crab = cards[0].clone();
 
@@ -340,7 +354,7 @@ mod tests {
             to: 15,   // a2
             figure: Figure::Pawn,
         };
-        let mov_result = state.make_move(&mov, &PlayerColor::Red, &crab);
+        let mov_result = state.make_move(&mov, PlayerColor::Red, 0);
 
         // Check if the game stays in progress after the move
         assert_eq!(mov_result, MoveResult::InProgress);
@@ -348,7 +362,8 @@ mod tests {
         assert_eq!(get_bit(state.pawns[PlayerColor::Red as usize], 15), 1);
         // a1 does not have a pawn
         assert_eq!(get_bit(state.pawns[PlayerColor::Red as usize], 20), 0);
-        // TODO: Check card rotation
+        // Check card rotation
+        assert_eq!(*state.deck.neutral_card(), crab)
     }
 
     #[test]
@@ -356,7 +371,7 @@ mod tests {
         let deck = Deck::new([CRAB, RABBIT, DRAGON, TIGER, FROG]);
         let mut state = State::with_deck(deck);
 
-        let cards = state.deck.get_player_cards(&PlayerColor::Blue);
+        let cards = state.deck.get_player_cards(PlayerColor::Blue);
         // Cloning to avoid immutable borrow before mutable
         let tiger = cards[1].clone();
 
@@ -365,7 +380,7 @@ mod tests {
             to: 11,  // b3
             figure: Figure::Pawn,
         };
-        let mov_result = state.make_move(&mov, &PlayerColor::Blue, &tiger);
+        let mov_result = state.make_move(&mov, PlayerColor::Blue, 1);
 
         // Check if the game stays in progress after the move
         assert_eq!(mov_result, MoveResult::InProgress);
@@ -373,7 +388,8 @@ mod tests {
         assert_eq!(get_bit(state.pawns[PlayerColor::Blue as usize], 11), 1);
         // b5 does not have a pawn
         assert_eq!(get_bit(state.pawns[PlayerColor::Blue as usize], 1), 0);
-        // TODO: Check card rotation
+        // Check card rotation
+        assert_eq!(*state.deck.neutral_card(), tiger);
     }
 
     #[test]
@@ -398,7 +414,7 @@ mod tests {
         */
         state.pawns[PlayerColor::Blue as usize] = 0x5801_0000;
 
-        let cards = state.deck.get_player_cards(&PlayerColor::Red);
+        let cards = state.deck.get_player_cards(PlayerColor::Red);
         // Cloning to avoid immutable borrow before mutable
         let crab = cards[0].clone();
 
@@ -407,16 +423,16 @@ mod tests {
             to: 15,   // a2
             figure: Figure::Pawn,
         };
-        let mov_result = state.make_move(&mov, &PlayerColor::Red, &crab);
+        let mov_result = state.make_move(&mov, PlayerColor::Red, 0);
 
         // Check if the game stays in progress after the move
         assert_eq!(mov_result, MoveResult::Capture);
-        // b3 has a pawn
+        // a2 has a pawn
         assert_eq!(get_bit(state.pawns[PlayerColor::Red as usize], 15), 1);
-        // b5 does not have a pawn
+        // a1 does not have a pawn
         assert_eq!(get_bit(state.pawns[PlayerColor::Red as usize], 20), 0);
-        // TODO: Check card rotation
-
+        // Check card rotation
+        assert_eq!(*state.deck.neutral_card(), crab);
         // Check if blue has only 3 pawns on b5, d5 and e5
         assert_eq!(state.pawns[PlayerColor::Blue as usize], 0x5800_0000);
     }
@@ -443,7 +459,7 @@ mod tests {
         */
         state.pawns[PlayerColor::Blue as usize] = 0x5820_0000;
 
-        let cards = state.deck.get_player_cards(&PlayerColor::Blue);
+        let cards = state.deck.get_player_cards(PlayerColor::Blue);
         // Cloning to avoid immutable borrow before mutable
         let tiger = cards[1].clone();
 
@@ -452,7 +468,7 @@ mod tests {
             to: 20,   // a1
             figure: Figure::Pawn,
         };
-        let mov_result = state.make_move(&mov, &PlayerColor::Blue, &tiger);
+        let mov_result = state.make_move(&mov, PlayerColor::Blue, 1);
 
         // Check if the game stays in progress after the move
         assert_eq!(mov_result, MoveResult::Capture);
@@ -460,8 +476,8 @@ mod tests {
         assert_eq!(get_bit(state.pawns[PlayerColor::Blue as usize], 20), 1);
         // a3 does not have a pawn
         assert_eq!(get_bit(state.pawns[PlayerColor::Blue as usize], 10), 0);
-        // TODO: Check card rotation
-
+        // Check card rotation
+        assert_eq!(*state.deck.neutral_card(), tiger);
         // Check if red has only 3 pawns on b1, d1 and e1
         assert_eq!(state.pawns[PlayerColor::Red as usize], 0x0000_0580);
     }
@@ -488,7 +504,7 @@ mod tests {
         */
         state.pawns[PlayerColor::Red as usize] = 0x0100_0680;
 
-        let cards = state.deck.get_player_cards(&PlayerColor::Red);
+        let cards = state.deck.get_player_cards(PlayerColor::Red);
         // Cloning to avoid immutable borrow before mutable
         let crab = cards[0].clone();
 
@@ -497,7 +513,7 @@ mod tests {
             to: 2,   // c5
             figure: Figure::Pawn,
         };
-        let mov_result = state.make_move(&mov, &PlayerColor::Red, &crab);
+        let mov_result = state.make_move(&mov, PlayerColor::Red, 0);
 
         // Check if the game stays in progress after the move
         assert_eq!(mov_result, MoveResult::RedWin);
@@ -505,8 +521,8 @@ mod tests {
         assert_eq!(get_bit(state.pawns[PlayerColor::Red as usize], 2), 1);
         // b5 does not have a pawn
         assert_eq!(get_bit(state.pawns[PlayerColor::Red as usize], 7), 0);
-        // TODO: Check card rotation
-
+        // Check card rotation
+        assert_eq!(*state.deck.neutral_card(), crab);
         // Check if blue king is dead
         assert_eq!(state.kings[PlayerColor::Blue as usize], 0);
     }
@@ -533,7 +549,7 @@ mod tests {
         */
         state.pawns[PlayerColor::Blue as usize] = 0x5808_0000;
 
-        let cards = state.deck.get_player_cards(&PlayerColor::Blue);
+        let cards = state.deck.get_player_cards(PlayerColor::Blue);
         // Cloning to avoid immutable borrow before mutable
         let tiger = cards[1].clone();
 
@@ -542,7 +558,7 @@ mod tests {
             to: 22,   // c1
             figure: Figure::Pawn,
         };
-        let mov_result = state.make_move(&mov, &PlayerColor::Blue, &tiger);
+        let mov_result = state.make_move(&mov, PlayerColor::Blue, 1);
 
         // Check if the game stays in progress after the move
         assert_eq!(mov_result, MoveResult::BlueWin);
@@ -550,8 +566,8 @@ mod tests {
         assert_eq!(get_bit(state.pawns[PlayerColor::Blue as usize], 22), 1);
         // a3 does not have a pawn
         assert_eq!(get_bit(state.pawns[PlayerColor::Blue as usize], 12), 0);
-        // TODO: Check card rotation
-
+        // Check card rotation
+        assert_eq!(*state.deck.neutral_card(), tiger);
         // Check if red king is dead
         assert_eq!(state.kings[PlayerColor::Red as usize], 0);
     }
