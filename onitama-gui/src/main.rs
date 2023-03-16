493fa84e3fe46c93e5e40d2b5b6346e7d53a7ea8
cyoq <di9akoz@gmail.com>
2023-03-16 22:07:11 +0200

    Add egui library to build gui

---
+++ onitama-gui/src/main.rs
@@ -0,0 +1,21 @@
+use eframe::{run_native, NativeOptions};
+use egui::{Vec2, Visuals};
+use onitama::Onitama;
+
+pub mod onitama;
+
+fn main() -> Result<(), eframe::Error> {
+    tracing_subscriber::fmt::init();
+
+    run_native(
+        "Onitama",
+        NativeOptions {
+            initial_window_size: Some(Vec2::new(1800.0, 840.0)),
+            ..Default::default()
+        },
+        Box::new(|cc| {
+            cc.egui_ctx.set_visuals(Visuals::light());
+            Box::new(Onitama::new(cc))
+        }),
+    )
+}
---
+++ onitama-gui/src/onitama.rs
@@ -0,0 +1,195 @@
+use eframe::{App, CreationContext};
+use egui::{
+    Button, CentralPanel, Color32, Context, Direction, FontData, FontDefinitions, FontFamily,
+    Hyperlink, Label, Layout, Pos2, Rect, RichText, SidePanel, TopBottomPanel, Ui,
+};
+use egui_extras::{Column, Size, StripBuilder, TableBuilder};
+
+const UTILITY_PANEL_WIDTH: f32 = 370.;
+const BOARD_PANEL_WIDTH: f32 = 930.;
+const PADDING: f32 = 15.;
+// const UTILITY_PANEL_HEIGHT: f32 = 500.;
+// const HISTORY_PANEL_HEIGHT: f32 = 340.;
+
+#[derive(Debug)]
+pub struct Onitama;
+
+impl Default for Onitama {
+    fn default() -> Self {
+        Self
+    }
+}
+
+impl Onitama {
+    pub fn new(cc: &CreationContext) -> Self {
+        Onitama::configure_fonts(&cc.egui_ctx);
+
+        Self
+    }
+
+    fn configure_fonts(ctx: &Context) {
+        let mut font_def = FontDefinitions::default();
+
+        // load the font
+        font_def.font_data.insert(
+            "MesloLGS".to_string(),
+            FontData::from_static(include_bytes!("../MesloLGS_NF_Regular.ttf")),
+        );
+
+        // set the font to be in the first priority
+        font_def
+            .families
+            .get_mut(&FontFamily::Proportional)
+            .expect("Proportional key must be in the 'families' property")
+            .insert(0, "MesloLGS".to_string());
+
+        // load the fonts to the context
+        ctx.set_fonts(font_def);
+    }
+
+    fn board_panel(&self, ui: &mut Ui) {
+        let faded_color = ui.visuals().window_fill();
+        let faded_color = |color: Color32| -> Color32 {
+            use egui::Rgba;
+            egui::lerp(Rgba::from(color)..=Rgba::from(faded_color), 0.5).into()
+        };
+
+        ui.horizontal(|ui| {
+            let background = Rect::from_points(&[
+                Pos2::new(0., 0.),
+                Pos2::new(750., 0.),
+                Pos2::new(0., 750.),
+                Pos2::new(750., 750.),
+            ]);
+            ui.painter().rect_filled(background, 0.0, Color32::BLACK);
+            StripBuilder::new(ui)
+                .size(Size::exact(150.0))
+                .vertical(|mut strip| {
+                    // Create a builder
+                    strip.strip(|builder| {
+                        // Create rows
+                        builder.sizes(Size::exact(150.), 5).vertical(|mut strip| {
+                            for _row in 0..5 {
+                                // Create column builder
+                                strip.strip(|builder| {
+                                    // Create columns
+                                    builder.sizes(Size::exact(150.), 5).horizontal(|mut strip| {
+                                        for _col in 0..5 {
+                                            strip.cell(|ui| {
+                                                let rect = ui.available_rect_before_wrap();
+                                                ui.painter().rect_filled(
+                                                    rect,
+                                                    0.0,
+                                                    faded_color(Color32::RED),
+                                                );
+                                                // ui.add(Button::new(format!("{}, {}", _row, _col)));
+                                                ui.label("width: 50%\nheight: remaining");
+                                            });
+                                        }
+                                    });
+                                });
+                            }
+                        });
+                    });
+                });
+        });
+    }
+
+    fn deck_panel(ui: &mut Ui) {}
+
+    fn utility_panel(&self, ui: &mut Ui) {
+        ui.with_layout(
+            egui::Layout::top_down(egui::Align::LEFT).with_cross_justify(true),
+            |ui| {
+                self.utility_widget(ui);
+                ui.add(egui::Separator::default().grow(8.0));
+                self.history_widget(ui);
+            },
+        );
+    }
+
+    fn utility_widget(&self, ui: &mut Ui) {
+        // Add button on the left for starting a new game
+        ui.with_layout(Layout::left_to_right(egui::Align::Min), |ui| {
+            ui.add(Button::new(
+                RichText::new("Start a new game").text_style(egui::TextStyle::Body),
+            ));
+
+            ui.add(Button::new(
+                RichText::new("Make a new setup").text_style(egui::TextStyle::Body),
+            ));
+        });
+        ui.add_space(PADDING);
+
+        // Add button on the right to open a window with a new game setup
+        // ui.with_layout(Layout::right_to_left(egui::Align::Max), |ui| {
+        //     ui.add(Button::new(
+        //         RichText::new("Make a new setup").text_style(egui::TextStyle::Body),
+        //     ));
+        // });
+
+        // ui.add_space(PADDING);
+
+        // // Add Statistics heading
+        ui.vertical_centered(|ui| {
+            ui.add(Label::new(
+                RichText::new("Statistics").text_style(egui::TextStyle::Heading),
+            ))
+        });
+        ui.add_space(PADDING);
+
+        // Add labels for the statistics
+        ui.add(Label::new("Evaluation Score: OVER9000"));
+        ui.add_space(PADDING);
+
+        // Add footer with links
+        ui.separator();
+        ui.vertical_centered(|ui| {
+            ui.add_space(PADDING);
+            // add a link to the egui framework
+            ui.add(Hyperlink::from_label_and_url(
+                RichText::new("Made with egui").text_style(egui::TextStyle::Monospace),
+                "https://github.com/emilk/egui",
+            ));
+            // then we'll put github link the the headlines source code
+            ui.add(Hyperlink::new("https://github.com/"));
+
+            ui.add_space(10.);
+        });
+        ui.add_space(PADDING);
+    }
+
+    fn history_widget(&self, ui: &mut Ui) {
+        ui.vertical_centered_justified(|ui| {
+            ui.add(Label::new(
+                RichText::new("History").text_style(egui::TextStyle::Heading),
+            ));
+        });
+    }
+}
+
+impl App for Onitama {
+    fn update(&mut self, ctx: &Context, _frame: &mut eframe::Frame) {
+        // ctx.set_debug_on_hover(true);
+        SidePanel::new(egui::panel::Side::Left, "board_panel")
+            .max_width(BOARD_PANEL_WIDTH)
+            .min_width(BOARD_PANEL_WIDTH)
+            .resizable(false)
+            .show(ctx, |ui| {
+                ui.with_layout(Layout::centered_and_justified(Direction::TopDown), |ui| {
+                    self.board_panel(ui);
+                });
+            });
+
+        CentralPanel::default().show(ctx, |ui| {
+            ui.label("Hello world!");
+            ui.label("It is me, deck panel!");
+        });
+
+        SidePanel::new(egui::panel::Side::Right, "right_panel")
+            .max_width(UTILITY_PANEL_WIDTH)
+            .min_width(UTILITY_PANEL_WIDTH)
+            .resizable(false)
+            .show(ctx, |ui| self.utility_panel(ui));
+    }
+}
