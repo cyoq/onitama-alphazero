c985270cc164414d29cc7982eabde043fccd7c80
cyoq <di9akoz@gmail.com>
2023-03-19 12:42:03 +0200

    Make GameBoard struct as container and not widget

--- onitama-gui/src/game_board/mod.rs
+++ onitama-gui/src/game_board/mod.rs
@@ -1,6 +1,6 @@
 pub mod cell;
 
-use egui::{Color32, Widget};
+use egui::Color32;
 use onitama_game::{
     common::{from_2d_to_1d, get_bit},
     game::{player_color::PlayerColor, state::State},
@@ -19,41 +19,12 @@ pub struct GameBoard<'a> {
     pub cell_size: f32,
 }
 
-impl<'a> Widget for GameBoard<'a> {
-    fn ui(self, ui: &mut egui::Ui) -> egui::Response {
+impl<'a> GameBoard<'a> {
+    pub fn show(&self, ui: &mut egui::Ui) {
         let GameBoard { state, cell_size } = self;
-        // Widget code can be broken up in four steps:
-        //  1. Decide a size for the widget
-        //  2. Allocate space for it
-        //  3. Handle interactions with the widget (if any)
-        //  4. Paint the widget
-
-        // 1. Deciding widget size:
-        // You can query the `ui` how much space is available,
-        // but in this example we have a fixed size widget based on the height of a standard button:
-        let desired_size = egui::vec2(cell_size * 5., cell_size * 5.);
-
-        // 2. Allocating space:
-        // This is where we get a region of the screen assigned.
-        // We also tell the Ui to sense clicks in the allocated region.
-        // let (rect, mut response) = ui.allocate_exact_size(desired_size, egui::Sense::click());
-        let mut response = ui.allocate_response(egui::vec2(0., 0.), egui::Sense::click());
-
-        // 3. Interact: Time to check for clicks!
-        if response.clicked() {
-            response.mark_changed(); // report back that the value changed
-        }
-
-        // Attach some meta-data to the response which can be used by screen readers:
-        // response.widget_info(|| egui::WidgetInfo::selected(egui::WidgetType::Checkbox, *on, ""));
-
-        // 4. Paint!
-        // Make sure we need to paint:
-        // if !ui.is_rect_visible(rect) {
-        //     return response;
-        // }
 
         let mut bg_fill = BG_FILL;
+
         egui::Grid::new("game_board")
             .min_col_width(0.)
             .min_row_height(0.)
@@ -75,14 +46,10 @@ impl<'a> Widget for GameBoard<'a> {
                             bg_fill = BG_FILL;
                         }
 
-                        ui.add(self::cell::Cell::new(row, col, bg_fill, cell_size));
+                        ui.add(self::cell::Cell::new(row, col, bg_fill, *cell_size));
                     }
                     ui.end_row();
                 }
             });
-
-        // All done! Return the interaction response so the user can check what happened
-        // (hovered, clicked, ...) and maybe show a tooltip:
-        response
     }
 }
