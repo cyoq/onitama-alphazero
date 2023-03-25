020b8c4f73fa825a935fc457911a10a294e07194
cyoq <di9akoz@gmail.com>
2023-03-25 11:43:36 +0200

    Improve AlphaBeta evaluation function

--- onitama-game/src/ai/alpha_beta/evaluation.rs
+++ onitama-game/src/ai/alpha_beta/evaluation.rs
@@ -9,12 +9,12 @@ use crate::{
 };
 
 // PST is taken from: https://github.com/maxbennedich/onitama/blob/master/src/main/java/onitama/ai/evaluation/PieceSquareTables.java#L10
-const PIECE_SQUARE_TABLE: [[i32; 5]; 5] = [
-    [0, 4, 8, 4, 0],
-    [4, 8, 12, 8, 4],
-    [8, 12, 16, 12, 8],
-    [4, 8, 12, 8, 4],
-    [0, 4, 8, 4, 0],
+const PIECE_SQUARE_TABLE: [i32; 25] = [
+    0, 4, 8, 4, 0, //
+    4, 8, 12, 8, 4, //
+    8, 12, 16, 12, 8, //
+    4, 8, 12, 8, 4, //
+    0, 4, 8, 4, 0,
 ];
 
 #[derive(Debug)]
@@ -58,8 +58,8 @@ impl Evaluation {
             (count_bits(enemy_pawns) * 10 + count_bits(enemy_king) * 10000) as i32;
 
         // how far king is from the enemy temple
-        let enemy_distance_to_temple = Self::manhattan_distance(enemy_king_pos, my_temple as u32);
-        let my_distance_to_temple = Self::manhattan_distance(my_king_pos, enemy_temple as u32);
+        let enemy_distance_to_temple = Self::distance(enemy_king_pos, my_temple as u32);
+        let my_distance_to_temple = Self::distance(my_king_pos, enemy_temple as u32);
 
         let mut me_close_to_enemy_king = 0;
         let mut enemies_close_to_my_king = 0;
@@ -72,24 +72,36 @@ impl Evaluation {
             let enemy_pawn = get_bit(enemy_pawns, n);
 
             if my_pawn == 1 {
-                me_close_to_enemy_king += Self::manhattan_distance(my_pawn, enemy_king_pos);
-                let (x, y) = Move::convert_to_2d(my_pawn);
-                my_piece_square += PIECE_SQUARE_TABLE[y as usize][x as usize];
+                me_close_to_enemy_king += Self::distance(my_pawn, enemy_king_pos);
+                my_piece_square += PIECE_SQUARE_TABLE[n];
             } else if enemy_pawn == 1 {
-                enemies_close_to_my_king += Self::manhattan_distance(enemy_pawn, my_king_pos);
-                let (x, y) = Move::convert_to_2d(enemy_pawn);
-                enemy_piece_square += PIECE_SQUARE_TABLE[y as usize][x as usize];
+                enemies_close_to_my_king += Self::distance(enemy_pawn, my_king_pos);
+                enemy_piece_square += PIECE_SQUARE_TABLE[n];
             }
         }
+
         sign * ((my_piece_score_sum - enemy_piece_score_sum)
-            - (my_distance_to_temple - enemy_distance_to_temple)
+            + (my_distance_to_temple - enemy_distance_to_temple)
             + (me_close_to_enemy_king - enemies_close_to_my_king)
             + (my_piece_square - enemy_piece_square))
     }
 
-    fn manhattan_distance(from: u32, to: u32) -> i32 {
+    fn distance(from: u32, to: u32) -> i32 {
         let (from_x, from_y) = Move::convert_to_2d(from);
         let (to_x, to_y) = Move::convert_to_2d(to);
+        Self::euclidean_distance(from_x, from_y, to_x, to_y)
+    }
+
+    #[allow(dead_code)]
+    #[inline]
+    fn manhattan_distance(from_x: u32, from_y: u32, to_x: u32, to_y: u32) -> i32 {
         (to_x as i32 - from_x as i32).abs() + (to_y as i32 - from_y as i32).abs()
+    }
+
+    #[allow(dead_code)]
+    #[inline]
+    fn euclidean_distance(from_x: u32, from_y: u32, to_x: u32, to_y: u32) -> i32 {
+        ((to_x as f32 - from_x as f32).powf(2.) + (to_y as f32 - from_y as f32).powf(2.)).sqrt()
+            as i32
     }
 }
