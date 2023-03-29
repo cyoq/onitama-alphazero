4e3f29dba5b3a9bddfeb46cf47ac5f5992a551ab
cyoq <di9akoz@gmail.com>
2023-03-29 08:44:17 +0300

    Add card show in setup menu

--- onitama-gui/src/setup_window.rs
+++ onitama-gui/src/setup_window.rs
@@ -1,6 +1,13 @@
 use egui::*;
 use egui_extras::{Size, StripBuilder};
+use onitama_game::game::{
+    card::{Card, CARD_NAMES, ORIGINAL_CARDS},
+    player_color::PlayerColor,
+};
 
+use crate::move_card::MoveCard;
+
+const MOVE_CARD_CELL_SIZE: f32 = 18.;
 const SETUP_WINDOW_WIDTH: f32 = 900.;
 const SETUP_WINDOW_HEIGHT: f32 = 550.;
 
@@ -14,7 +21,6 @@ impl SetupWindow {
             .min_width(SETUP_WINDOW_WIDTH)
             .min_height(SETUP_WINDOW_HEIGHT)
             .show(ctx, |ui| {
-                ui.label("text");
                 ui.separator();
                 Self::show_deck_panel(ui);
                 ui.separator();
@@ -25,6 +31,8 @@ impl SetupWindow {
     fn show_top_panel(&self, ui: &mut Ui) {}
 
     fn show_deck_panel(ui: &mut Ui) {
+        let deck = &ORIGINAL_CARDS;
+
         StripBuilder::new(ui)
             // Sizes for the card rows
             .size(Size::exact(130.))
@@ -38,15 +46,11 @@ impl SetupWindow {
                 // strip builder that will separate row into 8 columns
                 strip.strip(|builder| {
                     builder.sizes(Size::remainder(), 8).horizontal(|mut strip| {
-                        for i in 0..8 {
+                        for card in deck.iter().take(8) {
                             strip.cell(|ui| {
                                 ui.vertical_centered(|ui| {
-                                    ui.painter().rect_filled(
-                                        ui.available_rect_before_wrap(),
-                                        0.0,
-                                        Color32::YELLOW,
-                                    );
-                                    ui.label(format!("Cell {}", i));
+                                    let r = Self::add_card_to_ui(ui, card);
+                                    r.context_menu(Self::nested_menus);
                                 });
                             });
                         }
@@ -55,15 +59,11 @@ impl SetupWindow {
                 // Last row with 8 columns
                 strip.strip(|builder| {
                     builder.sizes(Size::remainder(), 8).horizontal(|mut strip| {
-                        for i in 0..8 {
+                        for card in deck.iter().skip(8) {
                             strip.cell(|ui| {
                                 ui.vertical_centered(|ui| {
-                                    ui.painter().rect_filled(
-                                        ui.available_rect_before_wrap(),
-                                        0.0,
-                                        Color32::BLUE,
-                                    );
-                                    ui.label(format!("Cell {}", i));
+                                    let r = Self::add_card_to_ui(ui, card);
+                                    r.context_menu(Self::nested_menus);
                                 });
                             });
                         }
@@ -77,5 +77,45 @@ impl SetupWindow {
             ui.button("Start a game");
             ui.checkbox(&mut true, "Save my choice");
         });
+    }
+
+    fn add_card_to_ui(ui: &mut Ui, card: &Card) -> Response {
+        let mut stroke_fill = Color32::BLACK;
+        // if deck.get_card_idx(&card) == self.selected_card.card_idx {
+        //     stroke_fill = match PlayerColor::Red {
+        //         PlayerColor::Red => Color32::RED,
+        //         PlayerColor::Blue => Color32::BLUE,
+        //     }
+        // }
+
+        let response = ui.add(MoveCard {
+            mirror: &false,
+            card: card,
+            name: CARD_NAMES[card.index],
+            cell_size: MOVE_CARD_CELL_SIZE,
+            stroke_fill,
+        });
+
+        if response.clicked() {
+            tracing::debug!("Selected card index");
+        }
+
+        if response.hovered() {
+            ui.ctx().set_cursor_icon(egui::CursorIcon::PointingHand);
+        }
+
+        response
+    }
+
+    fn nested_menus(ui: &mut Ui) {
+        if ui.button("Red").clicked() {
+            ui.close_menu();
+        }
+        if ui.button("Blue").clicked() {
+            ui.close_menu();
+        }
+        if ui.button("Neutral").clicked() {
+            ui.close_menu();
+        }
     }
 }
