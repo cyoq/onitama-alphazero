4affa2884cf362c9f24908a510a08c57b567ef5b
cyoq <di9akoz@gmail.com>
2023-05-25 16:49:50 +0300

    Add config to the start

--- alphazero-training/src/bin/train.rs
+++ alphazero-training/src/bin/train.rs
@@ -1,8 +1,39 @@
-use alphazero_training::train::{train, TrainConfig};
+use std::time::Duration;
+
+use alphazero_training::{
+    alphazero_mcts::AlphaZeroMctsConfig,
+    evaluator::EvaluatorConfig,
+    train::{train, TrainConfig},
+};
 
 fn main() {
+    // TODO: playing with the stable deck at the moment
+    // let deck = Some(Deck::new([
+    //     ORIGINAL_CARDS[0].clone(),
+    //     ORIGINAL_CARDS[1].clone(),
+    //     ORIGINAL_CARDS[2].clone(),
+    //     ORIGINAL_CARDS[3].clone(),
+    //     ORIGINAL_CARDS[4].clone(),
+    // ]));
+
+    // let train_config = TrainConfig {
+    //     iterations: 10,
+    //     ..Default::default()
+    // };
     let train_config = TrainConfig {
-        iterations: 10,
+        mcts_config: AlphaZeroMctsConfig {
+            search_time: Duration::from_millis(50),
+            max_playouts: 100,
+            ..Default::default()
+        },
+        iterations: 1,
+        self_play_game_amnt: 10,
+        save_checkpoint: 1,
+        evaluation_checkpoint: 1,
+        evaluator_config: EvaluatorConfig {
+            game_amnt: 5,
+            ..Default::default()
+        },
         ..Default::default()
     };
     match train(train_config) {
